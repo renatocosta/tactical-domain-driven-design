103317a738b76b76b9e824a2d6be351cdbecd76b
Renato Costa <renatocostahome@gmail.com>
2021-10-28 18:21:08 -0300

    Update pipeline

---
+++ Domains/HostProperties/Infrastructure/Domain/Services/ExternalPropertyCalendarManager.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Domains\HostProperties\Infrastructure\Domain\Services;
+
+use Domains\HostProperties\Domain\Model\Property\Property;
+use Domains\HostProperties\Domain\Services\PropertyCalendarManageable;
+
+final class ExternalPropertyCalendarManager implements PropertyCalendarManageable
+{
+
+    public function addCalendarTo(Property $property): void
+    {
+    
+    }
+}
